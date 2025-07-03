b59ef111a1187376ffcc046d31f5bdc204f24e8a
Thiagoszaa <Thiagoheniquesoares17@gmail.com>
2025-07-02 21:18:34 -0300

    Testando env2

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KE;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   console.error('Variáveis de ambiente não encontradas:', {
