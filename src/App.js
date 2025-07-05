a030917dc5c203f4ab4b5b035668ac7002acbabf
unknown <thiagohenriquesoares17@gmail.com>
2025-07-04 22:19:05 -0300

    mduando regex numero

--- src/App.js
+++ src/App.js
@@ -696,20 +696,18 @@ function App() {
                           sx={{ mb: 2 }}
                         />
                         
-                        <TextField
-                            label="Telefone"
-                            variant="outlined"
-                            fullWidth
-                            name="telefone"
-                            value={dadosCliente.telefone}
-                            onChange={handleDadosClienteChange}
-                            error={dadosCliente.telefone !== "" && !/^\(\d{2}\)\s?\d{9}$/.test(dadosCliente.telefone)}
-                            helperText={
-                              dadosCliente.telefone !== "" && !/^\(\d{2}\)\s?\d{9}$/.test(dadosCliente.telefone)
-                                ? "Digite no formato (DD) 912345678"
-                                : ""
-                            }
-                          />
+<TextField
+  label="Telefone"
+  value={dadosCliente.telefone}
+  onChange={(e) => setDadosCliente({ ...dadosCliente, telefone: e.target.value })}
+  error={dadosCliente.telefone !== "" && !/^\(\d{2}\)\s?\d{9}$/.test(dadosCliente.telefone)}
+  helperText={
+    dadosCliente.telefone !== "" && !/^\(\d{2}\)\s?\d{9}$/.test(dadosCliente.telefone)
+      ? "Formato esperado: (99) 999999999"
+      : ""
+  }
+/>
+
 
                       </Box>
                       
